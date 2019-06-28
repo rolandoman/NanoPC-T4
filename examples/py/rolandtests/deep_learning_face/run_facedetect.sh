1b348366dd0e07d839b07b95eb273fa9b475eeff
unknown <roland.clarke@gmail.com>
2019-06-28 09:04:33 -0700

    add a .sh file to run the python

---
+++ examples/py/rolandtests/deep_learning_face/run_facedetect.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+export ARCHFLAGS="-arch aarch64"
+export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3
+export VIRTUALENVWRAPPER_VIRTUALENV=/usr/local/bin/virtualenv
+export WORKON_HOME=~/.virtualenvs
+source /usr/local/bin/virtualenvwrapper.sh
+workon cv
+. setqt5env
+DISPLAY=localhost:10.0 python3 detect_faces.py --image rooster.jpg --prototxt deploy.prototxt.txt --model res10_300x300_ssd_iter_140000.caffemodel 
+deactivate
